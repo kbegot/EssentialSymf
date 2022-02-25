9394aa6cbbb9d344fec2b1329e3a8b2635fd8666
Jeanbonnevie <91051436+Jeanbonnevie@users.noreply.github.com>
2022-02-25 17:39:18 +0100

    ajout gestion role

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * @Route("admin/userslist", name = "userlist")
+     */
+    public function userList(UserRepository $users)
+    {
+        return $this->render('admin/userList.html.twig',['users'=>$users->findAll()]);
+    }
+
+
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -78,8 +78,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+        if (empty($roles))
+        {
+            // guarantee every user at least has ROLE_USER
+            $roles[0] = 'ROLE_USER';
+        }
 
         return array_unique($roles);
     }
