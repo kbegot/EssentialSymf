d579e65c099804f2830f92942622928d08f41a26
Jean Bonnevie <j.bonnevie@sio.local>
2022-03-04 10:51:08 +0100

    ajout champ register

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -18,6 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
+            ->add('nom')
+            ->add('prenom')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
