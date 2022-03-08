7c7e79b5fff01e630edf9362ea7a5bf70089880e
Kévin Bégot <begot.kevin@gmail.com>
2022-03-08 18:56:57 +0100

    ajout JS + userlist

---
+++ public/js/script.js
@@ -0,0 +1,90 @@
+let togg1 = document.querySelector('#togg1');
+let togg2 = document.querySelector('#togg2');
+let active1 = document.getElementById("active1");
+
+let eleveClasse = document.getElementById("eleve-classe");
+let profMatiere = document.getElementById("prof-matière");
+
+let valider = document.getElementById("valider");
+
+let d1 = document.getElementById("d1");
+let d2 = document.getElementById("d2");
+
+togg1.addEventListener("click", () => {
+  if(getComputedStyle(d1).display != "none"){
+    d1.style.display = "none";
+    if(getComputedStyle(d2).display != "none"){
+      d2.style.display = "none";
+    }
+  } else {
+    d1.style.display = "block";
+    d2.style.display = "none";
+  }
+})
+
+togg2.addEventListener("click", () => {
+  if(getComputedStyle(d2).display != "none"){
+    d2.style.display = "none";
+    if(getComputedStyle(d1).display != "none"){
+      d1.style.display = "none";
+    }
+  }
+  else {
+    d2.style.display = "block";
+    d1.style.display = "none";
+  }
+ 
+})
+
+/*active1.addEventListener("click", () => {
+  if(getComputedStyle(form1).display != "none"){
+    form1.style.display = "none";
+  }
+  else {
+    form1.style.display = "flex";
+  }
+ 
+})*/
+
+const btn = document.querySelector('#active1');
+btn.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name="color"]:checked');
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    if(values == "eleve"){
+      valider.style.display = "inline-block";
+      eleveClasse.style.display = "flex";
+      profMatiere.style.display = "none";
+    }
+});    
+
+btn.addEventListener('click', (event) => {
+    let checkboxes = document.querySelectorAll('input[name="color"]:checked');
+    let values = [];
+    checkboxes.forEach((checkbox) => {
+        values.push(checkbox.value);
+    });
+    if(values == "prof"){
+      valider.style.display = "inline-block";
+      profMatiere.style.display = "flex";
+      eleveClasse.style.display = "none";
+    }
+});    
+
+btn.addEventListener('click', (event) => {
+  let checkboxes = document.querySelectorAll('input[name="color"]:checked');
+  let values = [];
+  checkboxes.forEach((checkbox) => {
+      values.push(checkbox.value);
+  });
+  if(values == "admin"){
+    valider.style.display = "inline-block";
+    profMatiere.style.display = "none";
+    eleveClasse.style.display = "none";
+  }
+}); 
+
+
+
