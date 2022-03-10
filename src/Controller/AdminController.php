06f19c6f630fea7fbce1e94bf54847000ee1747f
Jeanbonnevie <91051436+Jeanbonnevie@users.noreply.github.com>
2022-03-11 00:21:02 +0100

    ajout route userEdit

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
@@ -27,6 +28,22 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/userList.html.twig',['users'=>$users->findAll()]);
     }
+
+    /**
+     * @route("admin/userlist/edit/{id}/{role}", name = "admin_useredit")
+     */
+    public function userEdit(UserRepository $users, $id, $role, EntityManagerInterface $entityManager)
+    {
+        $user = $users->find($id);
+        $user->setRoles([$role]);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('admin_userlist');
+
+    }
+
+
 
 
 
@@ -58,9 +75,9 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("admin/filelist/edit/{id}", name = "admin_filelist_edit")
+     * @Route("admin/fileedit/{id}", name = "admin_filelist_edit")
      */
-    public function filelistedit(RessourceRepository $ressources)
+    public function fileEdit(RessourceRepository $ressources)
     {
         $ressource = $ressources->find($id);
 
