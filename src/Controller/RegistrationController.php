4f0b61f73b3e4e38d7ab01270cf92ea2779a03cc
ThomasMouranchon <thomas.mouranchon@gmail.com>
2022-03-11 09:27:40 +0100

    Modif Login et Register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -35,7 +35,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
