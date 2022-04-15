8ec6c4da8f66ace37bc342cb10c644106e3e3f8a
Jeanbonnevie <bonnevie.jean7@gmail.com>
2022-04-15 21:33:25 +0200

    bouton supprimer matiere

--- public/js/script-classe.js
+++ public/js/script-classe.js
@@ -4,8 +4,55 @@ let listeEleve = document.getElementById('listeEleve');
 let listeMatiere = document.getElementById('listeMatiere');
 
 
-console.log(listeEleve);
-console.log(listeMatiere);
+//supprimer matières
+
+let bouttonSupprimerMatiere = document.querySelectorAll("#supprimer_matiere");
+
+bouttonSupprimerMatiere.forEach(button => {
+  
+  
+  button.addEventListener('click',(event)=>{
+    
+    var regularExpression= /(?<=\[).*?(?=\])/g;
+    let id = button.parentNode.parentNode.textContent.match(regularExpression)[0];
+    
+    //console.log(id);
+    if (window.confirm(`Vous vous apprêtez à supprimer cet matière`))
+    {
+      location.href= `matieredelete/${id}`; 
+    }
+    
+  });
+});
+
+
+/*let bouttonSupprimerEleve = document.querySelectorAll("#supprimer_eleve");
+
+bouttonSupprimerEleve.forEach(button => {
+  
+  
+  button.addEventListener('click',(event)=>{
+    
+    var regularExpression= /(?<=\[).*?(?=\])/g;
+    let id = button.parentNode.parentNode.textContent.match(regularExpression)[0];
+    
+    //console.log(id);
+    if (window.confirm(`Vous vous apprêtez à supprimer cet eleve`))
+    {
+      location.href= `userlist/edit/${id}/ `; 
+    }
+    
+  });
+});
+*/
+
+
+
+
+
+
+
+
 
 
 bouttonAfficherMatiere.forEach(button => {
@@ -25,13 +72,11 @@ bouttonAfficherMatiere.forEach(button => {
       
       Matieres = listeMatiere.getElementsByClassName('card-body')[0].querySelectorAll('.customer')
       Matieres.forEach(matiere => {
-        console.log(button);
-        console.log(button.getAttribute('idclasse'));
+
         // id == id classe
         matiereId = matiere.textContent.match(regularExpression)[0];
         classeId = matiere.textContent.match(regularExpression)[1];
-        console.log(matiereId);
-        console.log(classeId);
+
         if (classeId == button.getAttribute('idclasse')){
           matiere.style.display = 'block';
         }
@@ -52,65 +97,46 @@ bouttonAfficherMatiere.forEach(button => {
 });
 
 
-/*let togg3 = document.querySelectorAll('#togg3');
-let togg4 = document.querySelectorAll('#togg4');
+bouttonAfficherEleve.forEach(button => {
+  button.addEventListener("click", ()=>{
+    if (getComputedStyle(listeEleve).display != "none"){
+      listeEleve.style.display = "none";
 
-let d3 = document.getElementById("d3");
-let d4 = document.getElementById("d4");
-
-
-
-togg4.forEach(togg => {
-  
-    togg.addEventListener("click", () => {
-      
-      
-      if(getComputedStyle(d3).display != "none"){
-        d3.style.display = "none";
-        if(getComputedStyle(d4).display != "none"){
-          d4.style.display = "none";
-        }
-      } else {
-        d3.style.display = "block";
-        d4.style.display = "none";
+      if(getComputedStyle(listeMatiere).display != "none"){
+        listeMatiere.style.display = "none";
       }
-    })
+    }
+
+    else{
+      // affichage selon le classe id
+      var regularExpression= /(?<=\[).*?(?=\])/g;
+      
+      
+      Eleves = listeEleve.getElementsByClassName('card-body')[0].querySelectorAll('.customer')
+      Eleves.forEach(eleve => {
+
+        // id == id classe
+        eleveId = eleve.textContent.match(regularExpression)[0];
+        classeId = eleve.textContent.match(regularExpression)[1];
+
+        if (classeId == button.getAttribute('idclasse')){
+          eleve.style.display = 'block';
+        }
+
+        else{
+          eleve.style.display = 'none';
+        }
+        
+      });
+      
+      listeEleve.style.display = "block";
+      listeMatiere.style.display = "none";
+
+    }
+
+  })
+  
 });
 
 
-togg3.forEach(togg => {
 
-    togg.addEventListener("click", () => {
-      
-
-    if(getComputedStyle(d4).display != "none"){
-        d4.style.display = "none";
-        if(getComputedStyle(d3).display != "none"){
-        d3.style.display = "none";
-        }
-    }
-    else {
-      d4.style.display = "block";
-      d3.style.display = "none";
-    }
-    
-    })
-});*/
-
-
-
-
-/*//supprimer matières
-
-let supprimer2 = document.getElementById("supprimer");
-
-supprimer2.addEventListener('click',(event)=>{
-
-  var regularExpression= /(?<=\[).*?(?=\])/g;
-  let id = supprimer2.parentNode.parentNode.parentNode.querySelector('#matiere-id').textContent.match(regularExpression)[0];
-
-console.log(id);
-  if (window.confirm(`Vous vous apprêtez à supprimer cet utilisateur`))
-  {
-    location.href= `matieredelete/${id}`; 
-  };*/
