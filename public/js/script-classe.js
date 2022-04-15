13eeeb9dd34879439e29106543b11404154f464c
Jeanbonnevie <bonnevie.jean7@gmail.com>
2022-04-15 12:18:37 +0200

    grosse modif sur js classe / matiere

--- public/js/script-classe.js
+++ public/js/script-classe.js
@@ -1,4 +1,58 @@
-let togg3 = document.querySelectorAll('#togg3');
+let bouttonAfficherMatiere = document.querySelectorAll('#togg3');
+let bouttonAfficherEleve = document.querySelectorAll('#togg4');
+let listeEleve = document.getElementById('listeEleve');
+let listeMatiere = document.getElementById('listeMatiere');
+
+
+console.log(listeEleve);
+console.log(listeMatiere);
+
+
+bouttonAfficherMatiere.forEach(button => {
+  button.addEventListener("click", ()=>{
+    if (getComputedStyle(listeMatiere).display != "none"){
+      listeMatiere.style.display = "none";
+
+      if(getComputedStyle(listeEleve).display != "none"){
+        listeEleve.style.display = "none";
+      }
+    }
+
+    else{
+      // affichage selon le classe id
+      var regularExpression= /(?<=\[).*?(?=\])/g;
+      
+      
+      Matieres = listeMatiere.getElementsByClassName('card-body')[0].querySelectorAll('.customer')
+      Matieres.forEach(matiere => {
+        console.log(button);
+        console.log(button.getAttribute('idclasse'));
+        // id == id classe
+        matiereId = matiere.textContent.match(regularExpression)[0];
+        classeId = matiere.textContent.match(regularExpression)[1];
+        console.log(matiereId);
+        console.log(classeId);
+        if (classeId == button.getAttribute('idclasse')){
+          matiere.style.display = 'block';
+        }
+
+        else{
+          matiere.style.display = 'none';
+        }
+        
+      });
+      
+      listeMatiere.style.display = "block";
+      listeEleve.style.display = "none";
+
+    }
+
+  })
+  
+});
+
+
+/*let togg3 = document.querySelectorAll('#togg3');
 let togg4 = document.querySelectorAll('#togg4');
 
 let d3 = document.getElementById("d3");
@@ -39,4 +93,4 @@ togg3.forEach(togg => {
     }
     
     })
-});
+});*/
